a395db705ea6e4025834c637bf90f89d052b58fc
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2024-10-25 16:40:34 +0530

    Modify symbol layer for cljs

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
+`make git-submodule`
+
 `qmk compile -c -kb tmp/crkbd/rev4_1/standard -km manna-harbour_miryoku`
--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,3 +5,15 @@
 
 #pragma once
 
+
+#define MIRYOKU_LAYER_NUM \
+KC_LBRC,           KC_7,              KC_8,              KC_9,              KC_HASH,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
+KC_SCLN,           KC_4,              KC_5,              KC_6,              KC_EQL,            U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+KC_GRV,            KC_1,              KC_2,              KC_3,              KC_BSLS,           U_NA,              TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    KC_ALGR,           U_NA,              \
+U_NP,              U_NP,              KC_DOT,            KC_0,              KC_MINS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+#define MIRYOKU_LAYER_SYM \
+KC_ASTR,           KC_AMPR,           KC_LCBR,           KC_RCBR,           KC_CIRC,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
+KC_COLN,           KC_DLR,            KC_LPRN,           KC_RPRN,           KC_PLUS,           U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+KC_TILD,           KC_EXLM,           KC_LBRC,             KC_RCBR,           KC_PIPE,           U_NA,              TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  KC_ALGR,           U_NA,              \
+U_NP,              U_NP,              KC_PERC,           KC_AT,           KC_UNDS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
